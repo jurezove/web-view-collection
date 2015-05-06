2b33845c50d47518fce13631622081b8b3aab68e
Jure Žove <jure.pase.kure@gmail.com>
2015-05-06 10:14:27 +0200

    Initial Commit

---
+++ Web View in Collection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web View in Collection
+//
+//  Created by Jure Zove on 06/05/15.
+//  Copyright (c) 2015 Candy Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
