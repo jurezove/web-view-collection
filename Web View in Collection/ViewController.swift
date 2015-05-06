f927c4ea79d829bed2a178c2e7a69e1db62a4346
Jure Žove <jure.pase.kure@gmail.com>
2015-05-06 10:21:07 +0200

    Initial commit

--- Web View in Collection/ViewController.swift
+++ Web View in Collection/ViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.flowLayout.itemSize = self.view.bounds.size
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("WebViewCell", forIndexPath: indexPath) as! WebViewCell
+        cell.webView.loadRequest(NSURLRequest(URL: NSURL(string: "https://www.google.com/search?q=page+\(indexPath.row)")!))
+        return cell
+    }
+    
+    // Setting webview to nil, so the user doesn't see the old content
+    override func collectionView(collectionView: UICollectionView, didEndDisplayingCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+        (cell as! WebViewCell).webView.loadRequest(NSURLRequest(URL: NSURL(string: "about:blank")!))
+    }
+    
+    // Let's hide the status bar
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
 
---
+++ Web View in Collection/WebViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewCell.swift
+//  espn
+//
+//  Created by Jure Zove on 06/05/15.
+//  Copyright (c) 2015 Candy Code. All rights reserved.
+//
+
+import UIKit
+
+class WebViewCell: UICollectionViewCell {
+    @IBOutlet weak var webView: UIWebView!
+}
